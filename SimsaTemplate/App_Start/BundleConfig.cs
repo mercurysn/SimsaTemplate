c15ea161802f337bf2f05a7342d8351c6d4a1b00
mercurysn <sdhf452erh>
2015-05-30 17:41:38 +1000

    Changing BundleConfig to include DreamTemplate scripts

--- SimsaTemplate/App_Start/BundleConfig.cs
+++ SimsaTemplate/App_Start/BundleConfig.cs
@@ -8,11 +8,13 @@ namespace SimsaTemplate
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/bundles/scripts").Include(
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/jquery.validate*",
+                        "~/Scripts/custom-scripts.js",
+                        "~/Scripts/custom.js",
+                        "~/Scripts/jquery.metisMenu.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
@@ -25,6 +27,8 @@ namespace SimsaTemplate
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/Content/custom-styles.css",
+                      "~/Content/font-awesome.css",
                       "~/Content/site.css"));
         }
     }
